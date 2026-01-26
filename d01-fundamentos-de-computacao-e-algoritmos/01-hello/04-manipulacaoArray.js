6655b3466a30e67a31fa8ff554fdcb129617125a
unknown <user.name>
2026-01-26 19:26:08 -0300

    Corrige 04-manipulacaoArray.js

--- d01-fundamentos-de-computacao-e-algoritmos/01-hello/04-manipulacaoArray.js
+++ d01-fundamentos-de-computacao-e-algoritmos/01-hello/04-manipulacaoArray.js
@@ -5,10 +5,7 @@ const fibonacci = []; // criação do vetor atribuindo a constante fibonacci
 fibonacci[1] = 1; // manipulando o indice 1 do vetor atribuindo o valor 1
 fibonacci[2] = 1; // manipulando o indice 2 do vetor atribuindo o valor 1
 
-/*for(let i = 3; i < 20; i++)
+for(let i = 3; i < 20; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2] 
-console.log(fibonacci); 
-*/
-
-for(let i = 1; i < fibonacci.length; i++)
     console.log(fibonacci); 
+}
