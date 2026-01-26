8ef34fdf826e8cac7e25d44838723682b1bde0e5
Mateus de Moura Melo <user.name>
2026-01-26 20:10:13 -0300

    Ajuste 04-manipulacaoArray.js

--- d01-fundamentos-de-computacao-e-algoritmos/01-hello/04-manipulacaoArray.js
+++ d01-fundamentos-de-computacao-e-algoritmos/01-hello/04-manipulacaoArray.js
@@ -2,10 +2,13 @@ const fibonacci = []; // criação do vetor atribuindo a constante fibonacci
 
 //console.log(fibonacci); // retonará um vetor vazio
 
+fibonacci[0] = 0;
 fibonacci[1] = 1; // manipulando o indice 1 do vetor atribuindo o valor 1
 fibonacci[2] = 1; // manipulando o indice 2 do vetor atribuindo o valor 1
 
 for(let i = 3; i < 20; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2] 
-    console.log(fibonacci); 
 }
+
+console.log("Sequência de Fibonacci:")
+console.log(fibonacci); 
