a19d96be96af35a5354bfc4f156873c14423be41
Mateus de Moura Melo <user.name>
2026-01-31 15:49:13 -0300

    Atualiza code03-classes

--- d02-programacao-orientada-a-objetos/code03-classes/01_moduloCommonJS-Funcao/01a_consumidorCJS.js
+++ d02-programacao-orientada-a-objetos/code03-classes/01_moduloCommonJS-Funcao/01a_consumidorCJS.js
@@ -1,5 +1,6 @@
 const circulo = require('./01a_definicaoCJS.js');
 console.log(`Área do círculo de raio 4 é ${circulo.area(4)}`);
+console.log(`Circunferencia do círculo de raio 4 é ${circulo.circunferencia(4)}`);
 //desestruturando o objeto e acessando a função diretamente
-const {area} = require('./01a_definicaoCJS.js');
-console.log(`Área do círculo de raio 2 é ${area(2)}`);
+// const {area} = require('./01a_definicaoCJS.js');
+// console.log(`Área do círculo de raio 2 é ${area(2)}`);
--- d02-programacao-orientada-a-objetos/code03-classes/02_moduloCommonJS-Objeto/02a_consumidorCJS.js
+++ d02-programacao-orientada-a-objetos/code03-classes/02_moduloCommonJS-Objeto/02a_consumidorCJS.js
@@ -1,4 +1,5 @@
 //02a_consumidor
-const Circulo = require('./02a_definicaoCJS.js');
+const Circulo = require('./02a_definicaoCJS.njs');
 const c1 = new Circulo(4);
-console.log(`Área do círculo de raio 4 é ${c1.area()}`);
+console.log(`Área do círculo de raio 4 é ${c1.area().toFixed(2)}`);
+console.log(`Circunferência do círculo de raio 4 é ${c1.circunferencia().toFixed(2)}`);
